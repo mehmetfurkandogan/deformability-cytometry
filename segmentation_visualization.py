1d95a7fc55e5d3e0425efa0e2dc9edcb96b8f81c
dogan <mehmet.dogan@epfl.ch>
2023-12-31 11:11:29 +0100

    Minor changes

--- segmentation_visualization.py
+++ segmentation_visualization.py
@@ -1,8 +1,11 @@
+# This file is used to visualize the deformability cytometry segmentation results
+# Mehmet Furkan Doğan - December 2023
+######################################################################## IMPORT LIBRARIES
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.ticker import PercentFormatter
-
+######################################################################## MAIN
 # List of all input and output paths
 input_video_names = ['CellA_GFP',
                      'CellA_ShME480',
@@ -29,33 +32,33 @@ CellB_GFP = CellB_GFP[CellB_GFP['Porosity']<1.05]
 CellB_ME480 = CellB_ME480[CellB_ME480['Porosity']<1.05]
 
 
-# ########################## BRIGHNESS VS AREA ##########################
-# plt.scatter(CellA_GFP['Area (microns^2)'], CellA_GFP['Brightness'], label='CellA_GFP', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellA_ShME480['Area (microns^2)'], CellA_ShME480['Brightness'], label='CellA_ShME480', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellB_GFP['Area (microns^2)'], CellB_GFP['Brightness'], label='CellB_GFP', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellB_ME480['Area (microns^2)'], CellB_ME480['Brightness'], label='CellB_ME480', s=10, alpha=0.5, edgecolors='none')
+########################## BRIGHNESS VS AREA ##########################
+plt.scatter(CellA_GFP['Area (microns^2)'], CellA_GFP['Brightness'], label='CellA_GFP', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellA_ShME480['Area (microns^2)'], CellA_ShME480['Brightness'], label='CellA_ShME480', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellB_GFP['Area (microns^2)'], CellB_GFP['Brightness'], label='CellB_GFP', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellB_ME480['Area (microns^2)'], CellB_ME480['Brightness'], label='CellB_ME480', s=10, alpha=0.5, edgecolors='none')
 
-# plt.xlabel('Area [$\mu m^2$]')  # x-axis label
-# plt.ylabel('Brightness [a.u]')  # y-axis label
-# plt.legend()  # To show legend
-# # Save as eps
-# plt.savefig('Results\PartC\BrightnessVsArea.eps', format='eps')
-# # Save as pdf
-# plt.savefig('Results\PartC\BrightnessVsArea.pdf', format='pdf')
-# plt.show()
+plt.xlabel('Area [$\mu m^2$]')  # x-axis label
+plt.ylabel('Brightness [a.u]')  # y-axis label
+plt.legend()  # To show legend
+# Save as eps
+plt.savefig('Results\PartC\BrightnessVsArea.eps', format='eps')
+# Save as pdf
+plt.savefig('Results\PartC\BrightnessVsArea.pdf', format='pdf')
+plt.show()
 
-# ########################## DEFORMATION VS AREA ##########################
-# plt.scatter(CellA_GFP['Area (microns^2)'], CellA_GFP['Deformation'], label='CellA_GFP', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellA_ShME480['Area (microns^2)'], CellA_ShME480['Deformation'], label='CellA_ShME480', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellB_GFP['Area (microns^2)'], CellB_GFP['Deformation'], label='CellB_GFP', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellB_ME480['Area (microns^2)'], CellB_ME480['Deformation'], label='CellB_ME480', s=10, alpha=0.5, edgecolors='none')
+########################## DEFORMATION VS AREA ##########################
+plt.scatter(CellA_GFP['Area (microns^2)'], CellA_GFP['Deformation'], label='CellA_GFP', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellA_ShME480['Area (microns^2)'], CellA_ShME480['Deformation'], label='CellA_ShME480', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellB_GFP['Area (microns^2)'], CellB_GFP['Deformation'], label='CellB_GFP', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellB_ME480['Area (microns^2)'], CellB_ME480['Deformation'], label='CellB_ME480', s=10, alpha=0.5, edgecolors='none')
 
-# plt.xlabel('Area [$\mu m^2$]')  # x-axis label
-# plt.ylabel('Deformation')  # y-axis label
-# plt.legend()  # To show legend
-# # Save as pdf
-# plt.savefig('Results\PartC\DeformationVsArea.pdf', format='pdf')
-# plt.show()
+plt.xlabel('Area [$\mu m^2$]')  # x-axis label
+plt.ylabel('Deformation')  # y-axis label
+plt.legend()  # To show legend
+# Save as pdf
+plt.savefig('Results\PartC\DeformationVsArea.pdf', format='pdf')
+plt.show()
 
 ########################## POROSITY VS INERTIA RATIO ##########################
 plt.scatter(CellA_GFP['Porosity'], CellA_GFP['Inertia Ratio'], label='CellA_GFP', s=10, alpha=0.5, edgecolors='none')
@@ -70,30 +73,30 @@ plt.legend()  # To show legend
 # plt.savefig('Results\PartC\PorosityVsInertiaRatio.pdf', format='pdf')
 plt.show()
 
-# ########################## ASPECT RATIO VS AREA ##########################
-# plt.scatter(CellA_GFP['Area (microns^2)'], CellA_GFP['Aspect Ratio'], label='CellA_GFP', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellA_ShME480['Area (microns^2)'], CellA_ShME480['Aspect Ratio'], label='CellA_ShME480', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellB_GFP['Area (microns^2)'], CellB_GFP['Aspect Ratio'], label='CellB_GFP', s=10, alpha=0.5, edgecolors='none')
-# plt.scatter(CellB_ME480['Area (microns^2)'], CellB_ME480['Aspect Ratio'], label='CellB_ME480', s=10, alpha=0.5, edgecolors='none')
+########################## ASPECT RATIO VS AREA ##########################
+plt.scatter(CellA_GFP['Area (microns^2)'], CellA_GFP['Aspect Ratio'], label='CellA_GFP', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellA_ShME480['Area (microns^2)'], CellA_ShME480['Aspect Ratio'], label='CellA_ShME480', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellB_GFP['Area (microns^2)'], CellB_GFP['Aspect Ratio'], label='CellB_GFP', s=10, alpha=0.5, edgecolors='none')
+plt.scatter(CellB_ME480['Area (microns^2)'], CellB_ME480['Aspect Ratio'], label='CellB_ME480', s=10, alpha=0.5, edgecolors='none')
 
-# plt.xlabel('Area [$\mu m^2$]')  # x-axis label
-# plt.ylabel('Aspect Ratio')  # y-axis label
-# plt.legend()  # To show legend
-# # Save as pdf
-# plt.savefig('Results\PartC\AspectRatioVsArea.pdf', format='pdf')
-# plt.show()
+plt.xlabel('Area [$\mu m^2$]')  # x-axis label
+plt.ylabel('Aspect Ratio')  # y-axis label
+plt.legend()  # To show legend
+# Save as pdf
+plt.savefig('Results\PartC\AspectRatioVsArea.pdf', format='pdf')
+plt.show()
 
-# ########################## DEFORMATION HISTOGRAM ##########################
-# plt.hist(CellA_GFP['Deformation'], bins=50, label='CellA_GFP', alpha=0.5, density=True)
-# plt.hist(CellA_ShME480['Deformation'], bins=50, label='CellA_ShME480', alpha=0.5, density=True)
-# plt.hist(CellB_GFP['Deformation'], bins=50, label='CellB_GFP', alpha=0.5, density=True)
-# plt.hist(CellB_ME480['Deformation'], bins=50, label='CellB_ME480', alpha=0.5, density=True)
-# plt.gca().yaxis.set_major_formatter(PercentFormatter(xmax=100))
-# plt.xlim(0, 0.3)
+########################## DEFORMATION HISTOGRAM ##########################
+plt.hist(CellA_GFP['Deformation'], bins=50, label='CellA_GFP', alpha=0.5, density=True)
+plt.hist(CellA_ShME480['Deformation'], bins=50, label='CellA_ShME480', alpha=0.5, density=True)
+plt.hist(CellB_GFP['Deformation'], bins=50, label='CellB_GFP', alpha=0.5, density=True)
+plt.hist(CellB_ME480['Deformation'], bins=50, label='CellB_ME480', alpha=0.5, density=True)
+plt.gca().yaxis.set_major_formatter(PercentFormatter(xmax=100))
+plt.xlim(0, 0.3)
 
-# plt.xlabel('Deformation')  # x-axis label
-# plt.ylabel('Frequency')  # y-axis label
-# plt.legend()  # To show legend
-# # Save as pdf
-# plt.savefig('Results\PartC\DeformationHistogram.pdf', format='pdf')
+plt.xlabel('Deformation')  # x-axis label
+plt.ylabel('Frequency')  # y-axis label
+plt.legend()  # To show legend
+# Save as pdf
+plt.savefig('Results\PartC\DeformationHistogram.pdf', format='pdf')
 plt.show()
